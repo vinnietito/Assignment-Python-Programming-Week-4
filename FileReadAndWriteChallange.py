258cc1bb7215cce6c00a8a11cb65c4ab99c2a3cc
vinnietito <vincentnjuguna69@gmail.com>
2024-11-26 07:37:53 +0300

    Commits

--- FileReadAndWriteChallange.py
+++ FileReadAndWriteChallange.py
@@ -2,3 +2,11 @@ def modify_file(input_file, output_file):
     try:
         with open(input_file, 'r') as infile:
             content = infile.read()
+            
+            #modify Content: COnvert to uppercase
+            modified_content = content.upper()
+            
+        with open(output_file, 'w') as outfile:
+            outfile.write(modified_content)
+            
+        print
