30800e74b54cdfb0b1399b351ee3a1d0be757843
vinnietito <vincentnjuguna69@gmail.com>
2024-11-26 07:46:17 +0300

    Commits

--- FileReadAndWriteChallange.py
+++ FileReadAndWriteChallange.py
@@ -12,3 +12,5 @@ def modify_file(input_file, output_file):
         print(f"Content from {input_file} has been modified and written to {output_file}.")
     except FileNotFoundError:
         print(f"Error: The file '{input_file}' does not exist.")
+    except Exception as e:
+        print(f"An error occured: {e}")
