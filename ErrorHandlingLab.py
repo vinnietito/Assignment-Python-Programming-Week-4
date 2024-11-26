7a530790c8ab09c0a032b558280f2c5993d79297
vinnietito <vincentnjuguna69@gmail.com>
2024-11-26 07:59:24 +0300

    Commits

--- ErrorHandlingLab.py
+++ ErrorHandlingLab.py
@@ -2,4 +2,11 @@
 def read_file_with_error_handling():
     filename = input("ENter the filename: ")
     
-    
+    try:
+        with open(filename, 'r') as file:
+            print("File content:")
+            print(file.read())
+    except FileNotFoundError:
+        print(f"Error: The file '{filename}' was not found.")
+    except PermissionError:
+        print
